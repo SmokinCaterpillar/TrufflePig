fde4075374fd71468756c79ac8b7cdfab887b116
Robert Meyer <robert.meyer@flixbus.com>
2018-02-12 23:49:45 +0100

    new now as pipeline!

--- trufflepig/model.py
+++ trufflepig/model.py
@@ -2,6 +2,7 @@ import logging
 
 import numpy as np
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.pipeline import FeatureUnion, Pipeline
 
 from gensim.models.lsimodel import LsiModel
 from gensim import corpora
@@ -44,28 +45,75 @@ class TopicModel(object):
         result = corpus2dense(projection, self.num_topics).T
         return result
 
+    def fit(self, data, y=None):
+        self.train(data.tokens)
+        return self
 
-def train_models(post_frame, topic_kwargs, regressor_kwargs,
+    def transform(self, data):
+        return self.project_dense(data.tokens)
+
+
+class FeatureSelector(object):
+    def __init__(self, features):
+        self.features = features
+
+    def fit(self, X=None, y=None):
+        return self
+
+    def transform(self, data):
+        return data.loc[:, self.features]
+
+
+def train_pipeline(post_frame, topic_kwargs, regressor_kwargs,
                  features=FEATURES, targets=TARGETS):
 
-    logger.info('Training topic model with {}'.format(topic_kwargs))
-    topic_model = TopicModel(**topic_kwargs)
-    topic_model.train(post_frame.tokens)
+    logger.info('Training pipeline...')
+    target_frame = post_frame.loc[:, targets]
 
-    X_topic = topic_model.project_dense(post_frame.tokens)
-    X_style = post_frame.loc[:, features].values
-    X = np.concatenate((X_topic, X_style), axis=1)
+    feature_generation = FeatureUnion(
+        transformer_list=[
+            ('topic_model', TopicModel(**topic_kwargs)),
+            ('feature_selection', FeatureSelector(features))
+        ]
+    )
 
-    logger.info('Training data shape {}'.format(X.shape))
+    pipeline = Pipeline(steps=[
+            ('feature_generation', feature_generation),
+            ('regressor', RandomForestRegressor(**regressor_kwargs))
+        ]
+    )
 
-    Y = post_frame.loc[:, targets].values
+    pipeline.fit(post_frame, target_frame)
 
-    logger.info('Target data shape {}.\nTraining regressor'.format(Y.shape))
+    score = pipeline.score(post_frame, target_frame)
+    logger.info('...Done! Training score {}'.format(score))
 
-    regressor = RandomForestRegressor(**regressor_kwargs)
-    regressor.fit(X, Y)
+    return pipeline
 
-    score = regressor.score(X, Y)
-    logger.info('Training score {}'.format(score))
 
-    return topic_model, regressor
+######## Deprecated ############
+
+# def train_models(post_frame, topic_kwargs, regressor_kwargs,
+#                  features=FEATURES, targets=TARGETS):
+#
+#     logger.info('Training topic model with {}'.format(topic_kwargs))
+#     topic_model = TopicModel(**topic_kwargs)
+#     topic_model.train(post_frame.tokens)
+#
+#     X_topic = topic_model.project_dense(post_frame.tokens)
+#     X_style = post_frame.loc[:, features].values
+#     X = np.concatenate((X_topic, X_style), axis=1)
+#
+#     logger.info('Training data shape {}'.format(X.shape))
+#
+#     Y = post_frame.loc[:, targets].values
+#
+#     logger.info('Target data shape {}.\nTraining regressor'.format(Y.shape))
+#
+#     regressor = RandomForestRegressor(**regressor_kwargs)
+#     regressor.fit(X, Y)
+#
+#     score = regressor.score(X, Y)
+#     logger.info('Training score {}'.format(score))
+#
+#     return topic_model, regressor
