13ff90f285feeb872415452c0d90f2fcd7149eb9
Robert Meyer <robert.meyer@flixbus.com>
2018-02-26 07:19:36 +0100

    Made test more likely to succeed

--- integration_tests/bchain/getdata_test.py
+++ integration_tests/bchain/getdata_test.py
@@ -90,7 +90,7 @@ def test_scrape_or_load_data_parallel(temp_dir, steem_kwargs):
 
 def test_scrape_recent_date(steem_kwargs):
     frame = tpbg.scrape_hour_data(steem_kwargs,
-                                  stop_after=10,
+                                  stop_after=50,
                                   ncores=1)
     assert len(frame)
 
