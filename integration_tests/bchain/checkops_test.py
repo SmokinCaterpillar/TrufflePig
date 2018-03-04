89a0a60fde9b8882e7e76b21f6db82d988d6fd4d
Robert Meyer <robert.meyer@flixbus.com>
2018-03-04 23:13:06 +0100

    testing slightly different times

--- integration_tests/bchain/checkops_test.py
+++ integration_tests/bchain/checkops_test.py
@@ -6,8 +6,8 @@ import trufflepig.bchain.checkops as tpcd
 
 
 def test_check_all_ops_between(steem):
-    start = pd.to_datetime('2018-01-17-13:39:00')
-    end = pd.to_datetime('2018-01-17-13:41:00')
+    start = pd.to_datetime('2018-01-17-13:38:30')
+    end = pd.to_datetime('2018-01-17-13:41:10')
     comments = tpcd.check_all_ops_between(start, end, steem,
                                        account='originalworks',
                                        stop_after=25)
