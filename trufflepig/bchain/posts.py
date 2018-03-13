55c0ff8a25307de4cb98e190a6aa604ec146494f
Robert Meyer <robert.meyer@flixbus.com>
2018-03-13 15:52:31 +0100

    reduced memory reqs and better delegation stuff!

--- trufflepig/bchain/posts.py
+++ trufflepig/bchain/posts.py
@@ -84,7 +84,7 @@ def simple_topN_list(topN_authors, topN_permalinks, topN_titles,
     return result_string
 
 
-def get_delegation_link(steem_per_mvests, steem_powers=(1, 5, 10, 50, 100, 500, 1000, 5000)):
+def get_delegation_link(steem_per_mvests, steem_powers=(2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000)):
     """Returns a dictionary of links to delegate SP"""
     link_dict = {}
     for steem_power in steem_powers:
@@ -129,7 +129,7 @@ By checking, upvoting, and resteeming the found truffles from above, you help mi
 
 If you feel generous, you can delegate Steem Power to me and boost my daily upvotes on the truffle posts. In return, I will provide you with a *small* compensation for your trust in me and your locked Steem Power. **Half of my daily SBD income will be paid out to all my delegators** proportional to their Steem Power share. Payouts will start 3 days after your delegation.
 
-Click on one of the following links to delegate **[1]({sp1}), [5]({sp5}), [10]({sp10}), [50]({sp50}), [100]({sp100}), [500]({sp500}), [1000]({sp1000}),** or even **[5000 Steem Power]({sp5000})**. Thank You!
+Click on one of the following links to delegate **[2]({sp2}), [5]({sp5}), [10]({sp10}), [20]({sp20}), [50]({sp50}), [100]({sp100}), [200]({sp200}), [500]({sp500}), [1000]({sp1000}), [2000]({sp2000}),** or even **[5000 Steem Power]({sp5000})**. Thank You!
 
 Cheers,
 
@@ -390,7 +390,7 @@ If you feel generous, you can delegate Steem Power to me and boost my daily upvo
 
 Big thank you to the people who already delegated Power to me: {delegator_list}!
 
-Click on one of the following links to delegate **[1]({sp1}), [5]({sp5}), [10]({sp10}), [50]({sp50}), [100]({sp100}), [500]({sp500}), [1000]({sp1000}),** or even **[5000 Steem Power]({sp5000})**. Thank You!
+Click on one of the following links to delegate **[2]({sp2}), [5]({sp5}), [10]({sp10}), [20]({sp20}), [50]({sp50}), [100]({sp100}), [200]({sp200}), [500]({sp500}), [1000]({sp1000}), [2000]({sp2000}),** or even **[5000 Steem Power]({sp5000})**. Thank You!
 
 Cheers,
 
