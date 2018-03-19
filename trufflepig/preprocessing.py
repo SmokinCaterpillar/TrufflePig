63cf2c38049bc35a213928c464b05b24babb98e3
Robert Meyer <robert.meyer@flixbus.com>
2018-03-19 07:15:16 +0100

    added crypto passion

--- trufflepig/preprocessing.py
+++ trufflepig/preprocessing.py
@@ -27,7 +27,7 @@ FILTER_TAGS = ('mitnebcurationtrail', 'informationwar', 'truth', 'conspiracy',
 
 
 # Stay out of the whale wars!
-FILTER_AUTHORS = ('haejin', 'ew-and-patterns', 'caladium')
+FILTER_AUTHORS = ('haejin', 'ew-and-patterns', 'caladium', 'cryptopassion')
 
 
 def filter_duplicates(frame):
