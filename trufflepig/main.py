f930b1a232bfaea4d38500c2857d3fd1d97cc7d5
Robert Meyer <robert.meyer@flixbus.com>
2018-03-24 17:21:50 +0100

    moved max docs out of main loading before training

--- trufflepig/main.py
+++ trufflepig/main.py
@@ -24,7 +24,7 @@ from trufflepig.bchain.mpsteem import MPSteem
 logger = logging.getLogger(__name__)
 
 
-MAX_DOCUMENTS = 99000
+MAX_DOCUMENTS = 123000
 
 
 def parse_args():
@@ -51,7 +51,7 @@ def large_mp_preprocess(log_directory, current_datetime, steem, data_directory,
 
 def load_and_preprocess_2_frames(log_directory, current_datetime, steem,
                                  data_directory, offset_days=8,
-                                 days=7, days2=7, max_documents=MAX_DOCUMENTS):
+                                 days=7, days2=7):
     """ Function to load and preprocess the time span split into 2
     for better memory footprint
 
@@ -96,12 +96,6 @@ def load_and_preprocess_2_frames(log_directory, current_datetime, steem,
     post_frame.reset_index(inplace=True, drop=True)
     logger.info('Combining 2 frames into 1')
     post_frame = tppp.filter_duplicates(post_frame)
-
-    if len(post_frame) > max_documents:
-        logger.info('Frame has {} Documents, too many, '
-                    'reducing to {}'.format(len(post_frame), max_documents))
-        post_frame.sort_values('created', inplace=True, ascending=False)
-        post_frame = post_frame.iloc[:max_documents, :]
 
     logger.info('Searching for bid bots and bought votes')
     min_datetime = post_frame.created.min()
@@ -169,7 +163,15 @@ def main():
     topic_kwargs = dict(num_topics=128, no_below=7, no_above=0.1,
                         ngrams=(1,2), keep_n=333000)
 
-    pipeline = tpmo.load_or_train_pipeline(post_frame, model_directoy,
+    if len(post_frame) > MAX_DOCUMENTS:
+        logger.info('Frame has {} Documents, too many, '
+                    'reducing to {}'.format(len(post_frame), MAX_DOCUMENTS))
+        post_frame.sort_values('created', inplace=True, ascending=False)
+        train_frame = post_frame.iloc[:MAX_DOCUMENTS, :]
+    else:
+        train_frame = post_frame
+
+    pipeline = tpmo.load_or_train_pipeline(train_frame, model_directoy,
                                            current_datetime,
                                            regressor_kwargs=regressor_kwargs,
                                            topic_kwargs=topic_kwargs,
